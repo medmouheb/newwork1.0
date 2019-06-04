8aa14900a4341170a16ba2e615ebb090ff135241
medmouheb <mohamedmouheb@gmail.com>
2019-06-04 18:11:07 +0100

    6/6/2019

---
+++ build/precache-manifest.1391b22f062eed332556983cc4ca5be1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "084a14e9444aca4546b0",
+    "url": "/static/js/main.3ffa5860.chunk.js"
+  },
+  {
+    "revision": "a37af74132a6b4e7cae5",
+    "url": "/static/js/2.e114a3f1.chunk.js"
+  },
+  {
+    "revision": "084a14e9444aca4546b0",
+    "url": "/static/css/main.4c1e05f3.chunk.css"
+  },
+  {
+    "revision": "a37af74132a6b4e7cae5",
+    "url": "/static/css/2.ef4ed22f.chunk.css"
+  },
+  {
+    "revision": "cab91c57faf762b967ae3d14a17c8ae9",
+    "url": "/index.html"
+  }
+];
